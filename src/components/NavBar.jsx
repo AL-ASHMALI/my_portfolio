6f20ac1a873c3d808b59311466edbbe38bbcb450
Rabie AL-Ashmali <rabiealashmali@gmail.com>
2023-06-04 23:33:55 +0100

    Hero section added and styled

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -49,14 +49,23 @@ const NavBar = () => {
       </ul>
       <div
         onClick={() => setNav(!nav)}
-        className="cursor-pointer pr-4 z-10 text-gray-500"
+        className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
       >
         {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
       </div>
 
-      <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-500">
-        <li>fwfwef</li>
-      </ul>
+      {nav && ( // if the nav is on/shown, show the navagation links when clicking the hamburger menu button
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-700 text-gray-400">
+          {links.map(({ link, id }) => (
+            <li
+              key={id}
+              className="px-4 cursor-pointer capitalize py-6 text-4xl hover:scale-105 duration-200"
+            >
+              {link}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
