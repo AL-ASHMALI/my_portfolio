a3eeeea90ebcce6b61a86019fb32dd2212935a3c
Rabie AL-Ashmali <rabiealashmali@gmail.com>
2023-06-05 13:16:13 +0100

    started working on the social links in the hero section

---
+++ src/components/SocialLinks.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutLineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+const SocialLinks = () => {
+  return (
+    <div className="flex flex-col top-[35%] left-0 fixed">
+      <ul>
+        <li className="flex justify-between items-center w-40 h-14 px-4 bg-gray-400">
+          <a href="" className="flex justify-between items-center w-full">
+            <>
+              LinkedIn <FaLinkedin size={35} />
+            </>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default SocialLinks;
