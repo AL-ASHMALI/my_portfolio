c91467f6f4dc9137c5292a3cc9452adba21f96fd
Rabie Al-Ashmali <codeyourfuture@Rabie.local>
2024-01-20 17:09:11 +0000

    code refactoring

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,10 +4,10 @@ const About = () => {
   return (
     <div
       name='about'
-      className='w-full h-1/2 bg-gradient-to-b from-gray-800 to-black text-white'
+      className='bg-gradient-to-b from-gray-800 to-black text-white'
     >
-      <div className='max-w-screen-lg md:p-5 mx-auto flex flex-col justify-center w-full h-full p-5 pt-16'>
-        <div className='pb-6'>
+      <div className='max-w-screen-lg md:p-5 mx-auto flex flex-col justify-center w-full h-full p-5 pt-11'>
+        <div className='pt-16'>
           <p className='text-4xl font-bold inline border-b-4 border-gray-500'>
             About
           </p>
